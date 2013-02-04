f7222a76ee602c3d2932233ec458fbebce454369
Nathan Caza <mastercactapus@gmail.com>
2013-02-03 22:28:06 -0600

    simple blinking led test

---
+++ light_controller.c
@@ -0,0 +1,17 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define F_CPU 1000000UL
+#include <util/delay.h>
+
+int main() {
+  
+  DDRA = _BV(PA1);
+
+  while(1) {
+    PORTA ^= _BV(PA1);
+    _delay_ms(5);
+  }
+
+}
+
