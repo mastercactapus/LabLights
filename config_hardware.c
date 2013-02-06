7188d0f42019dc294a7c4503114a12962b3ec2d5
Nathan Caza <mastercactapus@gmail.com>
2013-02-05 21:52:19 -0600

    added hardware config

---
+++ config_hardware.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <avr/io.h>
+#include "config_hardware.h"
+
+void setup_pwmAB(void) {
+	TCCR0A |= _BV(WGM00) | _BV(WGM01) | _BV(COM0A1);
+	TCCR0B |= _BV(COM0B1);
+	DDRB |= _BV(PB2);
+	DDRA |= _BV(PA7);
+}
+void set_pwm_a(uint8_t *pwm) {
+	OCR0A = *pwm;
+}
+void set_pwm_b(uint8_t *pwm) {
+	OCR0B = *pwm;
+}
+
+void start_pwmAB(void) {
+	stop_pwmAB();
+	TCCR0B |= _BV(CS00); //no prescaler - int clock
+}
+
+void stop_pwmAB(void) {
+	TCCR0B &= ~(_BV(CS00) | _BV(CS01) | _BV(CS02));
+}
---
+++ config_hardware.h
@@ -0,0 +1,6 @@
+
+void setup_pwmAB(void);
+void set_pwm_a(uint8_t *pwm);
+void set_pwm_b(uint8_t *pwm);
+void start_pwmAB(void);
+void stop_pwmAB(void);
