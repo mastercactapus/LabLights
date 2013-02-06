5625eeb38a113efb114cd5f27907631fb7ac55ef
Nathan Caza <mastercactapus@gmail.com>
2013-02-05 22:49:52 -0600

    fixed calculation error

--- control.c
+++ control.c
@@ -7,10 +7,10 @@ void fade_pwm_a(uint8_t *time, uint8_t *pwm_level) {
 	uint8_t up = 1;
 	if (*pwm_level < OCR0A) up = 0;
 
-	uint16_t delay = up ? ((*pwm_level - OCR0A) * 250 / *time) : ((OCR0A - *pwm_level) * 250 / *time);
+	uint16_t delay = up ? (*time * 100 / (*pwm_level - OCR0A)) : (*time * 100 / (OCR0A - *pwm_level));
 
 	if (OCR0A == *pwm_level) {
-		custom_delay(*time * 250);
+		custom_delay(*time * 100);
 	} else if (up) {
 			while (OCR0A < *pwm_level) {
 				OCR0A++;
@@ -27,10 +27,10 @@ void fade_pwm_b(uint8_t *time, uint8_t *pwm_level) {
 	uint8_t up = 1;
 	if (*pwm_level < OCR0B) up = 0;
 
-	uint16_t delay = up ? ((*pwm_level - OCR0B) * 250 / *time) : ((OCR0B - *pwm_level) * 250 / *time);
+	uint16_t delay = up ? ((*pwm_level - OCR0B) * 100 / *time) : ((OCR0B - *pwm_level) * 100 / *time);
 
 	if (OCR0B == *pwm_level) {
-		custom_delay(*time * 250);
+		custom_delay(*time * 100);
 	} else if (up) {
 			while (OCR0B < *pwm_level) {
 				OCR0B++;
@@ -54,6 +54,6 @@ void set_pwm_b(uint8_t *pwm_level) {
 void custom_delay(uint16_t itr) {
 	while (itr) {
 		itr--;
-		_delay_us(4);
+		_delay_us(10);
 	}
 }
