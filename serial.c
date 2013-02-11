372bfb6401326f7d22351fd8c680af511430d06b
Nathan Caza <mastercactapus@gmail.com>
2013-02-10 23:52:46 -0600

    made serial reads use interrupts

--- serial.c
+++ serial.c
@@ -14,23 +14,27 @@
 #define RX_PORT PORTB
 #define RX_PIN  PB1
 
+uint8_t data_flag = 0;
+uint8_t data_byte = 0;
 
 void setup_serial(void) {
   TX_PORT |= _BV(TX_PIN);
   DDRB |= _BV(TX_PIN);
+  PCMSK1 |= _BV(PCINT9);
+  GIMSK |= _BV(PCIE1);
 }
 
 void send_byte(uint8_t b)
 {
   cli();
   b = ~b;
-  TX_PORT &= ~(_BV(TX_PIN));            // start bit
-  for(uint8_t i = 10; i; i--){        // 10 bits
-    _delay_us( TX_DELAY );            // bit duration
+  TX_PORT &= ~(_BV(TX_PIN));     // start bit
+  for(uint8_t i = 10; i; i--){   // 10 bits
+    _delay_us( TX_DELAY );       // bit duration
     if(b & 1)
-      TX_PORT &= ~(_BV(TX_PIN));        // data bit 0
+      TX_PORT &= ~(_BV(TX_PIN)); // data bit 0
     else
-      TX_PORT |= _BV(TX_PIN);           // data bit 1 or stop bit
+      TX_PORT |= _BV(TX_PIN);    // data bit 1 or stop bit
     b >>= 1;
   }
   sei();
@@ -42,16 +46,27 @@ void send_bytes(uint8_t *buf, uint8_t len) {
   }
 }
 
-uint8_t read_byte(void)
-{
-	uint8_t out=0;
-	while (!(RX_PORT & _BV(RX_PIN)));
+ISR (PCINT0_vect) {
 	cli();
+
+	_read_byte();
+
+	sei();
+}
+
+uint8_t read_byte(void) {
+	while(!data_flag);
+	data_flag=0;
+	return data_byte;
+}
+
+void _read_byte(void)
+{
+	data_byte=0;
 	for (uint8_t i=10; i; i--){
 		_delay_us(RX_DELAY);
-		if (RX_PORT & _BV(i)) out |= (1 << i);
+		if (RX_PORT & _BV(i)) data_byte |= (1 << i);
 	}
-	out = ~out;
-	sei();
-	return out;
+	data_byte = ~data_byte;
+	data_flag = 1;
 }
--- serial.h
+++ serial.h
@@ -2,3 +2,8 @@ void setup_serial(void);
 void send_byte(uint8_t b);
 void send_bytes(uint8_t *buf, uint8_t len);
 uint8_t read_byte(void);
+void _read_byte(void);
+
+
+extern uint8_t data_flag;
+extern uint8_t data_byte;
