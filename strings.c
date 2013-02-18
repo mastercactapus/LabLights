d461c6ca4a26f516b8eb563296bae8e60528b4c0
Nathan Caza <mastercactapus@gmail.com>
2013-02-17 20:07:34 -0600

    added hex_value and hex_char functions

--- strings.c
+++ strings.c
@@ -1,12 +1,40 @@
 #include "strings.h"
 
+uint8_t hex_value(char chr) { //invalid values will return 0
+  if (chr>102) return 0; //lower case letters a-f
+  if (chr>=97) return chr - 87;
+
+  if (chr>71) return 0; //upper case letters A-F
+  if (chr>=65) return chr - 55;
+
+  if (chr>57) return 0; //numbers 0-9
+  if (chr>=48) return chr-48;
+
+  return 0; //not a-fA-F0-9 return 0
+}
+
+char hex_char(uint8_t val) {
+  if (val < 10) return (char)(val + 48);
+  if (val < 16) return (char)(val + 87);
+  return 0; //invalid return 0
+}
+
 uint8_t hex_to_uint8(char *str) {
   uint8_t out = 0;
-  if (str_len(str))
+  if (str_len(str) > 1) 
+    return 16 * hex_value(str[0]) + hex_value(str[1]);
+  return hex_value(str[0]);
 }
 
 void uint8_to_hex(char *buf, uint8_t num) {
-
+  if (num < 16) {
+    buf[0] = hex_char(num);
+    buf[1] = 0;
+  } else {
+    buf[0] = hex_char(num / 16);
+    buf[1] = hex_char(num % 16);
+    buf[2] = 0;
+  }
 }
 
 uint8_t scmp(char *str1, char *str2) {
--- strings.h
+++ strings.h
@@ -3,3 +3,5 @@ void get_word(char *buf, char *str);
 void str_len(char *str);
 uint8_t hex_to_uint8(char *str);
 void uint8_to_hex(char *buf, uint8_t num);
+uint8_t hex_value(char chr);
+char hex_char(uint8_t val);
